5bf8eacbc73f0d18547375748fa8a6292aed6c60
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2026-05-21 03:19:26 -0400

    Code: basic implementation of .NET WPF key test

---
+++ code/test/dotnet.wpf/Application/Main/Application.EntryPoint.cs
@@ -0,0 +1,13 @@
+namespace SA.Test.CommandLine.Main {
+    using System;
+    using System.Windows;
+
+    static class TestStart {
+        [STAThread]
+        static void Main() {
+            Application app = new SA.Agnostic.UI.AdvancedApplication<View.WindowMain>() {};
+            app.Run();
+        } //MainClass
+    } //class TestStart
+
+}
---
+++ code/test/dotnet.wpf/Application/View/WindowMain.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace SA.Test.CommandLine.View {
+    using System.Windows;
+    using System.Windows.Input;
+
+    public partial class WindowMain : Window {
+
+        public WindowMain() {
+            InitializeComponent();
+            PreviewKeyDown += (sender, eventArgs) => {
+                var key = eventArgs.SystemKey != Key.None
+                    ? eventArgs.SystemKey
+                    : eventArgs.Key;
+                int keyValue = (int)key;
+                listBoxOutput.Items.Add($"{key.ToString()}, Value: {keyValue:X2}, Decimal Value: {keyValue}");
+            };
+        } //WindowMain
+
+    } //class WindowMain
+
+}
